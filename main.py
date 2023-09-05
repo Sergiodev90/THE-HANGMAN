1c435d27ef1bbe2948e48142c11277087bacd10d
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2023-09-05 17:50:33 +0200

    adding an example to do the game

---
+++ main.py
@@ -0,0 +1,22 @@
+import random 
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+guess = input("Guess a letter: ").lower()
+
+
+for position in range(word_length):
+    letter = chosen_word[position]
+    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+        display[position] = letter
+
+print(display)
